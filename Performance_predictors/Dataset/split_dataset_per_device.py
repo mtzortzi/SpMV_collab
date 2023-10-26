528e89bd251ba84924a79e707e0cb3560a08cfc3
pmpakos <pmpakos@hotmail.com>
2023-10-26 18:03:15 +0300

    split per device

--- Performance_predictors/Dataset/split_dataset_per_device.py
+++ Performance_predictors/Dataset/split_dataset_per_device.py
@@ -1,4 +1,17 @@
 import pandas as pd
 
-header = pd.read_csv("./data/data_sample_2.csv")
-print(header)
+# header = pd.read_csv("./data/data_sample_2.csv")
+# print(header)
+
+def split_dataset_per_device(prefix, df):
+    for System in list(set(df['System'])):
+        sys_df = df[df['System']==System]
+        print('./data/' + prefix + '/' + prefix + '_' + System + '.csv')
+        sys_df.to_csv('./data/' + prefix + '/' + prefix + '_' + System + '.csv', index=False )
+        # break
+
+all_format_df  = pd.read_csv('./data/' + 'all_format_runs_March_2023.csv')
+split_dataset_per_device('all_format', all_format_df)
+
+best_format_df = pd.read_csv('./data/' + 'best_format_runs_March_2023.csv')
+split_dataset_per_device('best_format', best_format_df)
