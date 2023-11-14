7d962efd9ceafdcd96f7780d6a345591dda8e7b5
pmpakos <pmpakos@hotmail.com>
2023-11-14 11:24:14 +0200

    split dataset per device + per implementation

--- Performance_predictors/Dataset/split_dataset_per_device.py
+++ Performance_predictors/Dataset/split_dataset_per_device.py
@@ -8,13 +8,12 @@ def split_dataset_per_device(prefix, df):
         sys_df = df[df['System']==System]
         print('./data/' + prefix + '/' + prefix + '_' + System + '.csv')
         sys_df.to_csv('./data/' + prefix + '/' + prefix + '_' + System + '.csv', index=False )
-        # break
 
-all_format_df  = pd.read_csv('./data/' + 'all_format_runs_March_2023.csv')
-split_dataset_per_device('all_format', all_format_df)
+# all_format_df  = pd.read_csv('./data/' + 'all_format_runs_March_2023.csv')
+# split_dataset_per_device('all_format', all_format_df)
 
-best_format_df = pd.read_csv('./data/' + 'best_format_runs_March_2023.csv')
-split_dataset_per_device('best_format', best_format_df)
+# best_format_df = pd.read_csv('./data/' + 'best_format_runs_March_2023.csv')
+# split_dataset_per_device('best_format', best_format_df)
 
 
 def split_validation_dataset_per_device(prefix, df):
@@ -22,10 +21,36 @@ def split_validation_dataset_per_device(prefix, df):
         sys_df = df[df['System']==System]
         print('./data/validation/' + prefix + '/' + prefix + '_' + System + '.csv')
         sys_df.to_csv('./data/validation/' + prefix + '/' + prefix + '_' + System + '.csv', index=False )
-        # break
+
+# all_format_df  = pd.read_csv('./data/validation/' + 'all_format_validation_runs_March_2023.csv')
+# split_validation_dataset_per_device('all_format', all_format_df)
+
+# best_format_df = pd.read_csv('./data/validation/' + 'best_format_validation_runs_March_2023.csv')
+# split_validation_dataset_per_device('best_format', best_format_df)
+
+
+def split_dataset_per_device_per_implementation(prefix, df):
+    for System in list(set(df['System'])):
+        sys_df = df[df['System']==System]
+        for implementation in list(set(sys_df['implementation'])):
+            impl_sys_df = sys_df[sys_df['implementation']==implementation]
+            print('./data/' + prefix + '/' + prefix + '_' + System + '_' + implementation + '.csv')
+            impl_sys_df.to_csv('./data/' + prefix + '/' + prefix + '_' + System + '_' + implementation + '.csv', index=False )
+
+all_format_df  = pd.read_csv('./data/' + 'all_format_runs_March_2023.csv')
+split_dataset_per_device_per_implementation('all_format', all_format_df)
+
+# no need to do this for the 'best_format' dataset...
+
+def split_validation_dataset_per_device_per_implementation(prefix, df):
+    for System in list(set(df['System'])):
+        sys_df = df[df['System']==System]
+        for implementation in list(set(sys_df['implementation'])):
+            impl_sys_df = sys_df[sys_df['implementation']==implementation]
+            print('./data/validation/' + prefix + '/' + prefix + '_' + System + '_' + implementation + '.csv')
+            impl_sys_df.to_csv('./data/validation/' + prefix + '/' + prefix + '_' + System + '_' + implementation + '.csv', index=False )
 
 all_format_df  = pd.read_csv('./data/validation/' + 'all_format_validation_runs_March_2023.csv')
-split_validation_dataset_per_device('all_format', all_format_df)
+split_validation_dataset_per_device_per_implementation('all_format', all_format_df)
 
-best_format_df = pd.read_csv('./data/validation/' + 'best_format_validation_runs_March_2023.csv')
-split_validation_dataset_per_device('best_format', best_format_df)
+# no need to do this for the 'best_format' dataset...
