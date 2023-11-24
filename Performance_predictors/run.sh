ba56a21f936c2f971e76624fb1237916b8b2e571
avionot244 <aymericvinot38@gmail.com>
2023-11-24 10:43:52 +0100

    Runned mlp

--- Performance_predictors/run.sh
+++ Performance_predictors/run.sh
@@ -4,15 +4,7 @@ do
 
 done
 
-for implementation in AOCL CSR5 Vec-CSR Merge-CSR MKL-IE Naive-CSR SELL-C-s SparseX
-do
-    clear && python3 main.py -m mlp -s AMD-EPYC-24 -i ${implementation}
-
-done
-
-
-
-clear && python3 main.py -m svr -s AMD-EPYC-24 -c -i None
+clear && python3 main.py -m mlp -s AMD-EPYC-24 -i None -c
 
 # TODO run svr and tree with cache and without implementation
 # TODO run mlp on all
