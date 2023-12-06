c4df5f0781309045582f77985c7701dbb8254f38
avionot244 <aymericvinot38@gmail.com>
2023-12-06 10:59:34 +0100

    End of run for the bigger mlp -> not better than smaller mlp

--- Performance_predictors/MLP/globals.py
+++ Performance_predictors/MLP/globals.py
@@ -1,11 +1,11 @@
 import torch
 
-nb_epochs = 300
+nb_epochs = 150
 lr = 0.01
 loss_fn = torch.nn.MSELoss()
 activation_fn = torch.nn.Sigmoid()
 final_activation_fn = torch.nn.Identity()
-nb_hidden_layers = 4
+nb_hidden_layers = 2
 in_dimension = 7
 out_dimension = 2
 hidden_size = 20
